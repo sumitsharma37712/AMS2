8d4f3e2564d961ee560fda92cad708231a50de4c
Sumit Sharma <117111687+sumitsharma37712@users.noreply.github.com>
2023-10-09 19:53:43 +0530

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -75,7 +75,7 @@ const adminAuth=asyncHandler(async(req,res)=>{
     try{
       if(!user){
         res.status(400)
-        throw new Error(`someting wrong not found`)
+        throw new Error({error:`someting wrong not found`})
       }else if(await bcrypt.compare(await password, user.password)){(
         tdata = {
           id:user._id,
@@ -99,7 +99,7 @@ const adminAuth=asyncHandler(async(req,res)=>{
         // return res.send({data:user,token:token},req.session.email,console.log(req.session.email))  
       }else{
         res.status(400)
-        throw new Error(`someting wrong user and password not found`)
+        throw new Error({error:`someting wrong user and password not found`})
       }
     }catch(error){
       res.status(400).send(error)
